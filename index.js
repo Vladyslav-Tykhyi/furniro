ef10ad2e99282b96aaaafaaa1b942af44155a0b8
Vladyslav-Tykhyi <vladleonidovich01@gmail.com>
2025-03-02 08:14:50 +0000

    Deploying to gh-pages from @ Vladyslav-Tykhyi/furniro@11d82e93a75fbb789c142cf3f3937230d877eb8a 🚀

--- index.js
+++ index.js
@@ -1,2 +1,57 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+(function(){const c=document.createElement("link").relList;if(c&&c.supports&&c.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))s(i);new MutationObserver(i=>{for(const t of i)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function a(i){const t={};return i.integrity&&(t.integrity=i.integrity),i.referrerPolicy&&(t.referrerPolicy=i.referrerPolicy),i.crossOrigin==="use-credentials"?t.credentials="include":i.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(i){if(i.ep)return;i.ep=!0;const t=a(i);fetch(i.href,t)}})();const e="/furniro/assets/pagination-BcXqGTYQ.png",n=document.querySelector(".products-list"),y=[{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5},{image:e,title:"Syltherine",description:"Stylish cafe chair",price:25e5}];function u(l){return l.map(({image:c,title:a,description:s,price:i})=>`
+ <li class="products-list-item overlay-card">
+        <div class="overlay-wrapper">
+          <picture>
+            <source
+              srcset="
+                ./img/pagination/pagination.png    1x,
+                ./img/pagination/pagination@2x.png 2x
+              "
+              media="(max-width:320px)"
+            />
+            <img
+              src="${c}"
+              alt="Furniro-products"
+              class="furniture-right-img"
+            />
+          </picture>
+          <div class="products-item-dscrp">
+            <h3 class="products-dscrp-title">${a}</h3>
+            <p class="products-dscrp-text">${s}</p>
+            <p class="products-dscrp-price">Rp ${i}</p>
+          </div>
+          <div class="overlay-block">
+            <button class="overlay-btn shadow-inset-center" type="button">
+              Add to cart
+            </button>
+            <ul class="overlay-list">
+              <li class="overlay-item pulsate-fwd">
+                <a class="overlay-item-text" href="#"
+                  ><svg width="16" height="16" class="overlay-icon">
+                    <use href="./img/icons.svg#icon-share"></use>
+                  </svg>
+                  Share</a
+                >
+              </li>
+              <li class="overlay-item pulsate-fwd">
+                <a class="overlay-item-text" href="#">
+                  <svg width="14" height="14" class="overlay-icon">
+                    <use href="./img/icons.svg#icon-arrows"></use>
+                  </svg>
+                  Compare</a
+                >
+              </li>
+              <li class="overlay-item pulsate-fwd">
+                <a class="overlay-item-text" href="#">
+                  <svg width="16" height="16" class="overlay-icon">
+                    <use href="./img/icons.svg#icon-heart"></use>
+                  </svg>
+                  Like</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </li>
+  `).join("")}const r=document.querySelector(".pagination-btn");let h=0;const p=4,d=y.length;function f(){const l=Array.from(n.children),c=d-l.length;if(c<=0){r.disabled=!0,r.style.backgroundColor="red",r.style.color="#fff",r.textContent="There are no more products";return}const a=Math.min(p,c),s=h*p,i=s+a,t=y.slice(s,i);n.insertAdjacentHTML("beforeend",u(t)),h++,d-n.children.length===0&&(r.disabled=!0,r.style.backgroundColor="red",r.style.color="#fff",r.textContent="No more products available.")}r.addEventListener("click",f);
 //# sourceMappingURL=index.js.map
